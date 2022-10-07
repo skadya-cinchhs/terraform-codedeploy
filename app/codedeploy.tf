b08005bcb1ef3ee2d4bd518d0888367d7b96b625
skadya-cinchhs <111126561+skadya-cinchhs@users.noreply.github.com>
2022-10-07 17:00:57 +0530

    Update codedeploy.tf

--- app/codedeploy.tf
+++ app/codedeploy.tf
@@ -32,3 +32,8 @@ resource "aws_iam_policy" "deployer_policy" {
 }
 EOF
 }
+
+resource "aws_iam_role_policy_attachment" "AWSCodeDeployRole" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+  role       = aws_iam_policy.deployer_policy.name
+}
